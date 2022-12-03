0a605b690f887dc6dbc74245498a9ce03d3f9281
Toni Zeidler <toni.z@gmx.de>
2022-12-03 15:47:47 +0100

    chng re

--- pgsql_ej_lotto_001.sql
+++ pgsql_ej_lotto_001.sql
@@ -11,7 +11,6 @@ CREATE ROLE ej_lotto WITH
   ENCRYPTED PASSWORD 'SCRAM-SHA-256$4096:sP+kNtYPyOvs77EOZvGHhA==$Xr8RDnLe0xCrlikHwMe4auwoh681tvfJLOqEsxgHy4I=:crXTLQ59j7V1Q2GYch9Kgee6SishRkV7PASgHJwNN1w=';
 
 -- Database: ej_lotto
-
 -- DROP DATABASE IF EXISTS ej_lotto;
 
 CREATE DATABASE ej_lotto
@@ -25,11 +24,9 @@ CREATE DATABASE ej_lotto
 
 
 -- Table: public.ej_draw
-
 -- DROP TABLE IF EXISTS public.ej_draw;
 
-CREATE TABLE IF NOT EXISTS public.ej_draw
-(
+CREATE TABLE IF NOT EXISTS public.ej_draw (
     date date NOT NULL,
     n1 integer NOT NULL,
     n2 integer NOT NULL,
@@ -41,19 +38,14 @@ CREATE TABLE IF NOT EXISTS public.ej_draw
     CONSTRAINT ej_draw_pkey PRIMARY KEY (date)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.ej_draw
-    OWNER to ej_lotto;
+ALTER TABLE IF EXISTS public.ej_draw OWNER to ej_lotto;
 
 
 
 -- Table: public.ej_row
-
 -- DROP TABLE IF EXISTS public.ej_row;
 
-CREATE TABLE IF NOT EXISTS public.ej_row
-(
+CREATE TABLE IF NOT EXISTS public.ej_row (
     row_id integer NOT NULL DEFAULT nextval('ej_row_row_id_seq'::regclass),
     n1 integer NOT NULL,
     n2 integer NOT NULL,
@@ -63,35 +55,31 @@ CREATE TABLE IF NOT EXISTS public.ej_row
     CONSTRAINT ej_row_pkey PRIMARY KEY (row_id)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.ej_row
-    OWNER to ej_lotto;
+ALTER TABLE IF EXISTS public.ej_row OWNER to ej_lotto;
 
 
 -- PROCEDURE: public.pr_ej_all_rows()
-
 -- DROP PROCEDURE IF EXISTS public.pr_ej_all_rows();
 
 CREATE OR REPLACE PROCEDURE public.pr_ej_all_rows()
 LANGUAGE 'plpgsql'
 AS $BODY$
 DECLARE
-    aus int;
-	_z1 int;
-	_z2 int;
-	_z3 int;
-	_z4 int;
-	_z5 int;
+    _from int;
+	_n1 int;
+	_n2 int;
+	_n3 int;
+	_n4 int;
+	_n5 int;
   BEGIN
-  	aus := 50;
-	_z1 := 1;
-    FOR _z1 IN 1 .. aus - 4 LOOP
-		FOR _z2 IN _z1 + 1 .. aus - 3 LOOP
-			FOR _z3 IN _z2 + 1 .. aus - 2 LOOP
-				FOR _z4 IN _z3 + 1 .. aus - 1 LOOP
-					FOR _z5 IN _z4 + 1 .. aus LOOP
-						INSERT INTO ej_row (n1, n2, n3, n4, n5) VALUES (_z1, _z2, _z3, _z4, _z5);
+  	_from := 50;
+	_n1 := 1;
+    FOR _n1 IN 1 .. _from - 4 LOOP
+		FOR _z2 IN _n1 + 1 .. _from - 3 LOOP
+			FOR _n3 IN _n2 + 1 .. _from - 2 LOOP
+				FOR _n4 IN _n3 + 1 .. _from - 1 LOOP
+					FOR _n5 IN _n4 + 1 .. _from LOOP
+						INSERT INTO ej_row (n1, n2, n3, n4, n5) VALUES (_n1, _n2, _n3, _n4, _n5);
 					END LOOP;
 				END LOOP;
 			END LOOP;
